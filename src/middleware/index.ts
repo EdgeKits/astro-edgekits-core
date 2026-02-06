bacfacc9e8f73fb7be094b900a01f58a997102b9
Gary Stupak <27917237+reedycat@users.noreply.github.com>
2026-02-06 14:36:47 +0200

    fix:dub extention in a middleware file name fixed

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,7 +1,7 @@
 import { sequence } from 'astro:middleware'
 import { i18nMiddleware } from './i18n'
-import { localeMiddleware } from './localeToAstroLocals.ts'
-import { demoProtectionMiddleware } from './demoProtectionMiddleware.ts'
+import { localeMiddleware } from './localeToAstroLocals'
+import { demoProtectionMiddleware } from './demoProtectionMiddleware'
 
 export const onRequest = sequence(
   i18nMiddleware,
